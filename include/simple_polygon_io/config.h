47849708d6fc3f7c7b61ed6d585e86031b31e4a5
Joaquin Bejar <jb@taunais.com>
2023-11-18 08:07:29 +0100

    add config class to handle Polygon access

--- include/simple_polygon_io/config.h
+++ include/simple_polygon_io/config.h
@@ -4,7 +4,31 @@
 
 #ifndef SIMPLE_POLYGON_IO_CONFIG_H
 #define SIMPLE_POLYGON_IO_CONFIG_H
-namespace simple_polygon_io::config {
 
+#include <simple_config/config.h>
+#include <simple_logger/logger.h>
+#include <common/common.h>
+
+namespace simple_polygon_io::config {
+    class PolygonIOConfig : public simple_config::Config {
+    public:
+
+        bool validate() override;
+
+        [[nodiscard]] json to_json() const override;
+
+        void from_json(const json &j) override;
+
+        [[nodiscard]] std::string to_string() const override;
+
+
+    protected:
+        std::string m_polygon_api_url = common::get_env_variable_string("POLYGON_API_URL", "https://api.polygon.io");
+        std::string m_polygon_api_key = common::get_env_variable_string("POLYGON_API_KEY", "");
+
+    public:
+        // add a logger to the config
+        std::shared_ptr<simple_logger::Logger> logger = std::make_shared<simple_logger::Logger>(loglevel);
+    };
 }
 #endif //SIMPLE_POLYGON_IO_CONFIG_H
--- src/config.cpp
+++ src/config.cpp
@@ -5,4 +5,69 @@
 
 namespace simple_polygon_io::config {
 
+    /**
+     * Validates the PolygonIO configuration.
+     *
+     * This method checks if the Polygon API URL and API key are not empty.
+     * It logs an error and returns false if either of them is empty.
+     *
+     * @return true if both the API URL and key are non-empty, false otherwise.
+     */
+    bool PolygonIOConfig::validate() {
+        if (m_polygon_api_url.empty()) {
+            logger->send<simple_logger::LogLevel::ERROR>("POLYGON_API_URL is empty");
+            return false;
+        }
+        if (m_polygon_api_key.empty()) {
+            logger->send<simple_logger::LogLevel::ERROR>("POLYGON_API_KEY is empty");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Converts the configuration to JSON format.
+     *
+     * This method serializes the PolygonIO configuration settings into a JSON object.
+     *
+     * @return A JSON object containing the configuration settings.
+     */
+    json PolygonIOConfig::to_json() const {
+        json j;
+        j["m_polygon_api_url"] = m_polygon_api_url;
+        j["m_polygon_api_key"] = m_polygon_api_key;
+
+        return j;
+    }
+
+    /**
+     * Populates the configuration from a JSON object.
+     *
+     * This method deserializes the PolygonIO configuration settings from a given JSON object.
+     *
+     * @param j The JSON object containing the configuration settings.
+     */
+    void PolygonIOConfig::from_json(const json &j) {
+        try {
+            m_polygon_api_key = j.at("m_polygon_api_key").get<std::string>();
+            m_polygon_api_url = j.at("m_polygon_api_url").get<std::string>();
+        } catch (json::exception &e) {
+            logger->send<simple_logger::LogLevel::ERROR>("Error parsing PolygonIOConfig: " + std::string(e.what()));
+            throw e;
+        }
+    }
+
+    /**
+     * Converts the configuration to a string representation.
+     *
+     * This method provides a textual representation of the PolygonIO configuration settings,
+     * which includes the API URL and the API key.
+     *
+     * @return A string representing the configuration settings.
+     */
+    std::string PolygonIOConfig::to_string() const {
+        return (std::string) "PolygonIOConfig {" + " m_polygon_api_url='" + m_polygon_api_url + '\'' +
+               ", m_polygon_api_key='" + m_polygon_api_key + '\'' +
+               '}';
+    }
 }
